479a68453e94434d2d7be35bf2b23782547f54ec
DESKTOP-HO8N1AV\User <johnkdk609@yonsei.ac.kr>
2021-09-01 16:34:40 +0900

    선생님 피드백 받고 수정

--- src/main/java/com/example/lesson04/NewUserController.java
+++ src/main/java/com/example/lesson04/NewUserController.java
@@ -12,6 +12,9 @@ import com.example.lesson04.bo.NewUserBO;
 @RequestMapping("/lesson04")
 @Controller
 public class NewUserController {
+	
+	@Autowired
+	private NewUserBO newUserBO;
 
 	// 요청 URL : http://localhost/lesson04/ex01/1
 	@RequestMapping(method = RequestMethod.GET, path = "/ex01/1")
@@ -19,8 +22,6 @@ public class NewUserController {
 		return "lesson04/addUser";		// 여기서 리턴되는 값은 JSP의 경로이다. 설정 파일에 suffix 설정이 돼 있으므로 .jsp는 안 쓴다. prefix 도 설정돼 있다.
 	}
 	
-	@Autowired
-	private NewUserBO newUserBO;
 	
 	// 요청 URL : http://localhost/lesson04/ex01/add_user
 	//@RequestMapping(path = "/ex01/add_user", method = RequestMethod.POST)
@@ -32,6 +33,7 @@ public class NewUserController {
 			@RequestParam(value = "introduce", required=false) String introduce) {
 		// DB Insert
 		
+		newUserBO.addNewUserAsField(name, yyyymmdd, email, introduce);
 		
 		return "lesson04/afterAddUser";
 	}
